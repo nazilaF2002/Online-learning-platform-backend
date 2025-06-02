9b4b378389b538fff8381c53d3d22373dcf6fd8a
Nazila Faizzadah <nazilafaizzadah37@gmail.com>
2025-06-02 16:19:13 +0430

    db connected using Atlas

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import env from 'dotenv';
 env.config();
 import app from "./app.js";
+import dbconected from './db.js';
 
 const PORT = process.env.PORT || 5000;
 
+dbconected();
 app.listen(PORT,()=>{
     console.log(`APP  is runing on prot : ${PORT}`);
 })
